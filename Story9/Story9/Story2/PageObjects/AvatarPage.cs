88f0f0f2a57b62a66b7450756501160ed4165101
David Wang <pengw22804@hotmail.com>
2019-07-25 14:44:22 +1000

    Create AvatarPage.cs

---
+++ Story9/Story9/Story2/PageObjects/AvatarPage.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Story2.WrapperFactory;
+using System;
+using System.Threading;
+
+
+namespace Story2.PageObjects
+{
+    public class AvatarPage
+    {
+        public void AssertAvatarImage()
+        {
+            //waiting for loading 
+            WebDriverWait wait = new WebDriverWait(BrowserFactory.Driver, new TimeSpan(0, 0, 5));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/header/nav/div/ul/li[1]/a/img")));
+
+            //assert avatar image is presented
+            Assert.IsTrue(BrowserFactory.Driver.FindElement(By.XPath("/html/body/div[1]/header/nav/div/ul/li[1]/a/img")).Displayed);
+        }
+
+        public void LogoutAvatar()
+        {
+            //logout through URL
+            BrowserFactory.Driver.Url= "http://lyratesting2.co.nz/logout";       
+        }
+
+        public void AssertLoginScreen()
+        {
+            Thread.Sleep(3000);
+
+            //Assert current screen in login screen
+            String currentUrl = BrowserFactory.Driver.Url;
+            Assert.AreEqual(currentUrl, "http://lyratesting2.co.nz/login");        
+        }
+
+        public void ClickSetting()
+        {
+            //click avatar personal setting
+            BrowserFactory.Driver.Url = "http://lyratesting2.co.nz/settings/";
+        }
+
+    }
+}
