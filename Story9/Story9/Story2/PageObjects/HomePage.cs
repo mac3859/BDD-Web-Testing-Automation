f47f24501314e82a45fe7ece0d17313e9e9992e7
David Wang <pengw22804@hotmail.com>
2019-07-25 14:38:03 +1000

    Create HomePage.cs
    Home Page includes Login link

---
+++ Story9/Story9/Story2/PageObjects/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using Story2.WrapperFactory;
+using System;
+
+
+namespace Story2.PageObjects
+{
+    public class HomePage
+    {
+        [FindsBy(How = How.XPath, Using = "/html/body/div[1]/header/nav/div/ul/li[2]/a")]
+        private IWebElement loginLink;
+
+        public void SelectLogin()
+       {
+            WebDriverWait wait = new WebDriverWait(BrowserFactory.Driver, new TimeSpan(0, 0, 5));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[1]/header/nav/div/ul/li[2]/a")));
+
+            loginLink.Click();         
+        }
+    }
+}
