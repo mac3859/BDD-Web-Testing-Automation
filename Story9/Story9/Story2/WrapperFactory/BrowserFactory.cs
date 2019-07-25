fa9ecef2163f946aa160f2e3762fe26e4e345cc4
David Wang <pengw22804@hotmail.com>
2019-07-25 14:39:44 +1000

    Create BrowserFactory.cs
    Page Factory define page

---
+++ Story9/Story9/Story2/WrapperFactory/BrowserFactory.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace Story2.WrapperFactory
+{
+    class BrowserFactory
+    {
+        public static IWebDriver driver;
+
+        public static IWebDriver Driver
+        {
+            get { return driver; }          
+        }
+
+        public static void InitBrowser()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        public static void LoadApplication()
+        {
+            Driver.Navigate().GoToUrl("http://lyratesting2.co.nz/");
+        }
+
+        public static void CloseAllDriver()
+        {
+            Driver.Close();
+        }
+    }
+}
